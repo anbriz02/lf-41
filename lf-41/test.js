f513e6b27797feb7df6c195e87db9480c956cdcc
anbriz02 <55641032+anbriz02@users.noreply.github.com>
2019-11-25 10:51:22 -0500

    Add files via upload

---
+++ lf-41/test.js
@@ -0,0 +1,19 @@
+const readlineSync = require("readline-sync");
+
+
+const mitoin = 63360;
+const mitoft = 5280;
+const mitoyd = 1760;
+
+let num = 0
+let value = ""
+
+do  {
+  origin = Number(readlineSync.question("\nEnter an origin unit: "));
+  origin = Number(readlineSync.question("Enter an origin unit: "));
+  dest = Number(readlineSync.question("Enter a destination unit: "));
+  dest = Number(readlineSync.question("Enter a destination unit: "));
+  value = Number(readlineSync.question("Enter a value: "));
+} while (num < 0){
+
+}
